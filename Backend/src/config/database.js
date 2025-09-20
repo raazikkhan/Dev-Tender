11e1a478a720119a7a4318e4768167328cd452a8
Md Razik Khan <95939876+raazikkhan@users.noreply.github.com>
2025-09-20 12:29:14 +0530

    Delete Backend directory

--- Backend/src/config/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const connectDB = async () => {
-  await mongoose.connect(
-    "mongodb+srv://Razikkhan:razik108@cluster0.s7ecvia.mongodb.net/devTinder"
-  );
-};
-
-module.exports = connectDB;
